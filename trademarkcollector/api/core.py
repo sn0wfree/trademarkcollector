9bc9d8446417b1b573228fc7965abbe5278fb64b
sn0wfree <snowfreedom0815@gmail.com>
2019-08-10 13:36:33 +0800

    add some files and proxy mark 1

--- trademarkcollector/api/core.py
+++ trademarkcollector/api/core.py
@@ -69,6 +69,43 @@ class SBW(object):
         self.main_goto(xpath)
 
 
+def get_new_pop_windows_handle_code(brows, exists_windows_list):
+    handle_windows_list = list(set(brows.window_handles) - set(exists_windows))
+    if len(handle_windows_list) == 1:
+        return handle_windows_list[0]
+    else:
+        raise ValueError('popup window own more than one window, please recheck!')
+
+
+class WindowsHolder(object):
+    def __init__(self, brows):
+        self.brows = brows
+        self.window_handle_code_holder = dict()
+        self.exists = []
+
+    @property
+    def window_handles(self):
+        return self.brows.window_handles
+
+    def add(self, name):
+        new_pop = list(set(self.window_handles)  - set(self.exists))
+        if len(new_pop) == 1:
+            self.window_handle_code_holder[name] = new_pop
+            self.exists.append(new_pop)
+            # return new_pop[0]
+        elif len(new_pop) == 0 :
+            print('this is no new window popup!')
+        else:
+            raise ValueError('popup window own more than one window, please re-check!')
+
+
+
+
+        self.window_handle_code_holder[k] = v
+
+    def
+
+
 if __name__ == '__main__':
     web_address = 'http://sbj.cnipa.gov.cn/sbcx/'
     sbw = SBW(web_address)
@@ -78,14 +115,28 @@ if __name__ == '__main__':
     time.sleep(3)
     sbw.go_related_search()
 
+    Container.window_handle_code_holder['main_window_handle'] = sbw.spider.driver.current_window_handle
+
+    # main_window_handle = sbw.spider.driver.current_window_handle
+
     brows = sbw.spider.driver
     from trademarkcollector.subpages.related_search import *
 
     type_in_nation_category(brows, key=9)
     time.sleep(1)
     # type_in_related_group(brows, key='0901')
-    time.sleep(1)
+    # time.sleep(1)
     type_in_mark_name(brows, key='大发')
+    time.sleep(1)
 
+    click_search_botton_autosearch(brows)
+    Container.window_handle_code_holder['mark_search_result_window_handle_code'] = \
+        list(set(brows.window_handles) - set([main_window_handle]))[0]
+    # '42949967305'
+
+    # single_result_pages_window_handle_code = \
+    # list(set(brows.window_handles) - set([main_window_handle]) - set([mark_search_result_window_handle_code]))[0]
+    #
+    # brows.switch_to.window(single_result_pages_window_handle_code)
 
     pass
